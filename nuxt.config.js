e25ec23923247ead33d109310f3bd0d310cbcae4
AlbertoAlfredo <raioalfredo@gmail.com>
2021-07-06 21:22:01 -0300

    melhoria iniciais

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  router: {
+    base: 'http://github.com/AlbertoAlfredo/Eyevision'
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {

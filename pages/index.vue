6a7d4696fea9bf068a190a8864ab447f677aed99
AlbertoAlfredo <raioalfredo@gmail.com>
2021-07-14 17:43:28 -0300

    quase pronto a base

--- pages/index.vue
+++ pages/index.vue
@@ -37,21 +37,35 @@
           <nuxt-img src="/robo01.png" class="img-imagem-quemsomos" />
         </div>
       </div>
-      <div v-for="(indv, index) in equipe" :key="index" class="avatar-quemsomos-container">
-        <div class="avatar-quemsomos">
-          <div class="img-avatar">
-          <nuxt-img class="avatar-img" :src="indv.img" :alt="indv.alt" />
+      <hr />
+      <div class="container-quemsomos-equipe">
+        <div v-for="(indv, index) in equipe" :key="index" class="avatar-quemsomos-container">
+        <a :href="indv.linkedin">
+          <div class="avatar-quemsomos">
+            <div class="img-avatar">
+            <nuxt-img class="avatar-img" :src="indv.img" :alt="indv.alt" />
+            </div>
+            <div class="texto-avatar">
+            <h3 class="titulo-avatar">{{indv.titulo}}</h3>
+            <img class="icone-quemsomos" src="~assets/images/logo_in.png" alt="" />
+            </div>
           </div>
-          <div class="texto-avatar">
-          <h3 class="titulo-avatar">{{indv.titulo}}</h3>
-          <img src="~assets/images/logo_in.png" alt="">
-          </div>
+        </a>
         </div>
       </div>
     </div>
-    <div id="news" class="container">
-      <h1>Produtos</h1>
+    <div id="contato" class="container">
+      <div class="container-quemsomos" style="flex-direction:row-reverse;">
+        <div class="texto-quemsomos" style="text-align: left;">
+          <h1 style="font-size: 4vw;">Contato</h1>
+          <iframe style="width: 40vw; height: 60vw;" src="https://docs.google.com/forms/d/e/1FAIpQLSf76vHwaygqk-fbpnyQVlSHdAegrj_3ATpd6QoWBOxTAtuNjQ/viewform?embedded=true" frameborder="0" marginheight="0" marginwidth="0">Carregando…</iframe>
+        </div>
+        <div class="imagem-quemsomos">
+          <nuxt-img src="/robo02.png" class="img-imagem-quemsomos" />
+        </div>
     </div>
+    </div>
+    <Footer/>
   </div>
 </template>
 
@@ -74,11 +88,11 @@ export default Vue.extend({
         {img: "teste.png", alt: "", titulo: "Mussum Ipsum, cacilds vidis litro abertis.", text: "Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo tempor."},
       ],
       equipe: [
-        {img: "teste.png", alt: "", titulo: "Mussum Ipsum, cacilds"},
-        {img: "teste.png", alt: "", titulo: "Mussum Ipsum, cacilds"},
-        {img: "teste.png", alt: "", titulo: "Mussum Ipsum, cacilds"},
-        {img: "teste.png", alt: "", titulo: "Mussum Ipsum, cacilds"},
-        {img: "teste.png", alt: "", titulo: "Mussum Ipsum, cacilds"},
+        {img: "teste.png", alt: "", titulo: "Mussum Ipsum, cacilds", linkedin: "#"},
+        {img: "teste.png", alt: "", titulo: "Mussum Ipsum, cacilds", linkedin: "#"},
+        {img: "teste.png", alt: "", titulo: "Mussum Ipsum, cacilds", linkedin: "#"},
+        {img: "teste.png", alt: "", titulo: "Mussum Ipsum, cacilds", linkedin: "#"},
+        {img: "teste.png", alt: "", titulo: "Mussum Ipsum, cacilds", linkedin: "#"},
       ]
     }
   }
@@ -180,16 +194,34 @@ export default Vue.extend({
     width: 100%;
     height: auto;
   }
+  .container-quemsomos-equipe{
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+  }
+  .avatar-quemsomos-container{
+    margin: 10px;
+  }
   .titulo-avatar{
-    font-size: 3vw;
+    font-size: 2.5vw;
+    color: var(--main-color);
   }
   .img-avatar{
-    width: 20vw;
+    width: 13vw;
     height: auto;
   }
   .avatar-img{
     width: 100%;
     height: auto;
+    border-radius: 10px;
+  }
+  .texto-avatar{
+    display: flex;
+    width: 13vw;
+  }
+  .icone-quemsomos{
+    width: 3vw;
+    height: 3vw;
   }
   hr{
     height: 4vh;
